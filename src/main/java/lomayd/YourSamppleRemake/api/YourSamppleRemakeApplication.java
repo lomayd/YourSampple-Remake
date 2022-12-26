0b4c46f931d7ffc600196a2b1d77e5c5cf10a86d
lomayd <jinho10167@gmail.com>
2022-12-26 23:33:37 +0900

    feat: Project Initialize

---
+++ src/main/java/lomayd/YourSamppleRemake/api/YourSamppleRemakeApplication.java
@@ -0,0 +1,13 @@
+package lomayd.YourSamppleRemake.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourSamppleRemakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourSamppleRemakeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lomayd/YourSamppleRemake/YourSamppleRemakeApplicationTests.java
@@ -0,0 +1,13 @@
+package lomayd.YourSamppleRemake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourSamppleRemakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
