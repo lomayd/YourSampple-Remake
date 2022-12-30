bd8e05de0213911993f1ffdc88ecd991676a46f6
lomayd <jinho10167@gmail.com>
2022-12-30 21:49:33 +0900

    feat: AgreementPhoneSale Entity Add

---
+++ src/main/java/lomayd/YourSamppleRemake/api/domain/agreement/AgreementPhoneSale.java
@@ -0,0 +1,27 @@
+package lomayd.YourSamppleRemake.api.domain.agreement;
+
+import lomayd.YourSamppleRemake.api.domain.phone.Phone;
+import lomayd.YourSamppleRemake.api.domain.plan.Plan;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AgreementPhoneSale {
+    @Id
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Phone phone;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Plan plan;
+
+    @Column
+    private Integer sale;
+}
