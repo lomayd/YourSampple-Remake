14ea1cc8f38d8e28321cf87fe8ef755722948b04
lomayd <jinho10167@gmail.com>
2023-01-02 19:50:38 +0900

    refactor: Bill Entity

--- src/main/java/lomayd/YourSamppleRemake/api/domain/bill/controller/BillController.java
+++ src/main/java/lomayd/YourSamppleRemake/api/domain/bill/controller/BillController.java
@@ -1,4 +1,22 @@
 package lomayd.YourSamppleRemake.api.domain.bill.controller;
 
+import lomayd.YourSamppleRemake.api.domain.bill.dto.BillRequestDto;
+import lomayd.YourSamppleRemake.api.domain.bill.dto.BillResponseDto;
+import lomayd.YourSamppleRemake.api.domain.bill.service.BillService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
 public class BillController {
+    private final BillService billService;
+
+    @GetMapping("/bill")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<BillResponseDto.BillCalc> billCalc(@Validated @RequestBody BillRequestDto.BillCalc billCalc) throws Exception {
+        return ResponseEntity.ok(billService.billCalc(billCalc));
+    }
 }
