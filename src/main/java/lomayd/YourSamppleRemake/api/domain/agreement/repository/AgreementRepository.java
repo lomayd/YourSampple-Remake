445d7afaf31ec9f09b28e02daae17385e1d86d27
lomayd <jinho10167@gmail.com>
2023-01-05 21:16:29 +0900

    refactor: Agreement Entity Delete

--- src/main/java/lomayd/YourSamppleRemake/api/domain/agreement/repository/AgreementRepository.java
+++
@@ -1,7 +0,0 @@
-package lomayd.YourSamppleRemake.api.domain.agreement.repository;
-
-import lomayd.YourSamppleRemake.api.domain.agreement.Agreement;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AgreementRepository extends JpaRepository<Agreement, String> {
-}
